f3cea1f44b9725881fdea564bfeaea39cc206d12
arslanah99 <42077616+arslanah99@users.noreply.github.com>
2023-05-11 22:31:00 -0600

    made gallery

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -4,23 +4,28 @@ import '@fontsource/open-sans';
 import '@fontsource/open-sans/800.css';
 import '@fontsource/open-sans/600.css';
 import { SiMyanimelist } from 'react-icons/si';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import icon from '../../assets/icon.svg';
 import './App.css';
 import { Navbar } from './components/Navbar';
 import { Home } from './pages/Home';
-import { AnimeList } from './pages/AnimeList';
+import { AnimeList } from './pages/AnimeList/AnimeList';
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <Router>
-      <CssBaseline />
-      <Navbar />
-      <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/anime-list" element={<AnimeList />} />
-        </Routes>
-      </div>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <CssBaseline />
+        <Navbar />
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/anime-list" element={<AnimeList />} />
+          </Routes>
+        </div>
+      </Router>
+    </QueryClientProvider>
   );
 }
